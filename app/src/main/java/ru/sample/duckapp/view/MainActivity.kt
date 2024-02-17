5fc55391a48d12324ad718903dcad089df4462fd
Victor Smirnov <vityaman.dev@yandex.ru>
2024-02-17 10:08:59 +0300

    [duck] Downloaded a duck on button press with progress

--- app/src/main/java/ru/sample/duckapp/view/MainActivity.kt
+++ app/src/main/java/ru/sample/duckapp/view/MainActivity.kt
@@ -1,9 +1,12 @@
 package ru.sample.duckapp.view
 
 import android.os.Bundle
-import android.widget.ImageView
+import android.view.View
 import androidx.activity.viewModels
 import androidx.appcompat.app.AppCompatActivity
+import com.google.android.material.button.MaterialButton
+import com.google.android.material.imageview.ShapeableImageView
+import com.google.android.material.progressindicator.CircularProgressIndicator
 import ru.sample.duckapp.R
 import ru.sample.duckapp.viewmodel.MainViewModel
 
@@ -11,12 +14,22 @@ import ru.sample.duckapp.viewmodel.MainViewModel
 class MainActivity : AppCompatActivity() {
     private val model: MainViewModel by viewModels()
 
-    private val image: ImageView by lazy { findViewById(R.id.image_main) }
+    private val image: ShapeableImageView by lazy { findViewById(R.id.image_main) }
+    private val button: MaterialButton by lazy { findViewById(R.id.button_main) }
+    private val progress: CircularProgressIndicator by lazy { findViewById(R.id.progress_main) }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         setSupportActionBar(findViewById(R.id.toolbar_main))
-        model.duck.observe(this, image::setImageBitmap)
+        model.duck.observe(this) { bitmap ->
+            image.setImageBitmap(bitmap)
+            progress.visibility = View.INVISIBLE
+        }
+        button.setOnClickListener {
+            progress.visibility = View.VISIBLE
+            model.update()
+        }
+        button.callOnClick()
     }
 }
--- app/src/main/java/ru/sample/duckapp/viewmodel/MainViewModel.kt
+++ app/src/main/java/ru/sample/duckapp/viewmodel/MainViewModel.kt
@@ -1,18 +1,23 @@
 package ru.sample.duckapp.viewmodel
 
 import android.graphics.Bitmap
-import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
 import androidx.lifecycle.ViewModel
-import androidx.lifecycle.liveData
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.launch
 import retrofit2.await
 import ru.sample.duckapp.eo.RemoteImage
 import ru.sample.duckapp.infra.Api
 
 class MainViewModel : ViewModel() {
-    val duck: LiveData<Bitmap> = liveData {
-        val duck = Api.ducksApi.getRandomDuck().await()
-        val image = RemoteImage(duck.url())
-        val bitmap = image.download()
-        emit(bitmap)
+    val duck: MutableLiveData<Bitmap> = MutableLiveData()
+
+    fun update() {
+        viewModelScope.launch {
+            val duck = Api.ducksApi.getRandomDuck().await()
+            val image = RemoteImage(duck.url())
+            val bitmap = image.download()
+            this@MainViewModel.duck.value = bitmap
+        }
     }
 }
