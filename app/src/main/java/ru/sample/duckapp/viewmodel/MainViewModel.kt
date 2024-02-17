a92ca4f2baba27a012d4e626c6b673a70382c8ed
Victor Smirnov <vityaman.dev@yandex.ru>
2024-02-17 11:19:01 +0300

    [duck] Downloaded a duck by http code

--- app/src/main/java/ru/sample/duckapp/viewmodel/MainViewModel.kt
+++ app/src/main/java/ru/sample/duckapp/viewmodel/MainViewModel.kt
@@ -8,16 +8,33 @@ import kotlinx.coroutines.launch
 import retrofit2.await
 import ru.sample.duckapp.eo.RemoteImage
 import ru.sample.duckapp.infra.Api
+import java.io.FileNotFoundException
 
 class MainViewModel : ViewModel() {
+    var onUpdateStart: () -> Unit = {}
+    var onUpdateEnd: () -> Unit = {}
+
     val duck: MutableLiveData<Bitmap> = MutableLiveData()
+    val code: MutableLiveData<Int> = MutableLiveData()
+    val error: MutableLiveData<String> = MutableLiveData()
 
     fun update() {
         viewModelScope.launch {
-            val duck = Api.ducksApi.getRandomDuck().await()
-            val image = RemoteImage(duck.url())
-            val bitmap = image.download()
-            this@MainViewModel.duck.value = bitmap
+            try {
+                onUpdateStart()
+                val url = when (val code = code.value) {
+                    null -> Api.ducksApi.getRandomDuck().await().url()
+                    else -> Api.httpCodeDuckUrl(code)
+                }
+                val image = RemoteImage(url)
+                val bitmap = image.download()
+                this@MainViewModel.duck.value = bitmap
+            } catch (_: FileNotFoundException) {
+                this@MainViewModel.error.value = "No such HTTP Code"
+            } finally {
+                onUpdateEnd()
+            }
         }
     }
+
 }
